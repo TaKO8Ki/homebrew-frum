699229ae7775b462e94097c339d688e715b1071b
Takayuki <takoyaki0316@gmail.com>
2021-04-25 23:27:24 +0900

    frum v0.1.0-alpha.1 release

--- Formula/frum.rb
+++ Formula/frum.rb
@@ -1,17 +1,17 @@
 class Frum < Formula
   desc "Slightly fast and modern Ruby version manager written in Rust"
   homepage "https://github.com/TaKO8Ki/frum"
-  version "0.1.0-alpha0"
+  version "0.1.0-alpha1"
   license "MIT"
   head "https://github.com/TaKO8Ki/frum.git"
-  url_version = "0.1.0-alpha.0"
+  url_version = "0.1.0-alpha.1"
 
   if OS.mac?
     url "https://github.com/TaKO8Ki/frum/releases/download/v#{url_version}/frum-v#{url_version}-x86_64-apple-darwin.tar.gz"
-    sha256 "57ab2eff76ac54049e7537c730eb62271258ee5646d6ff2a821ff44943ffa415"
+    sha256 "96406f3ae74ef0ef5359055fd5eff3291037aa3b0667e110b22e2fd8ad6ba1a2"
   elsif OS.linux?
     url "https://github.com/TaKO8Ki/frum/releases/download/v#{url_version}/frum-v#{url_version}-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "a495ddf28e9cc7da20b9f823eff8cfa7248dc992a247ca5fb5c369cf5c8027ce"
+    sha256 "82530f26be8113c7dd1a43398ea2f45c1e06d4e1c0af203a236d81145015112c"
   end
 
   livecheck do
