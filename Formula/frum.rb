91a425905e23ccc61b84d92c356c433c3104bc08
Takayuki <takoyaki0316@gmail.com>
2021-04-25 18:59:27 +0900

    set binary url

--- Formula/frum.rb
+++ Formula/frum.rb
@@ -1,25 +1,28 @@
 class Frum < Formula
   desc "Slightly fast and modern Ruby version manager written in Rust"
   homepage "https://github.com/TaKO8Ki/frum"
-  url "https://github.com/TaKO8Ki/frum/archive/v0.1.0-alpha.0.tar.gz"
-  sha256 "5f2aa0f1fea5ffe780f86232edec9af3946d0b0540ad68a377387712ab24f11c"
+  version "0.1.0-alpha.0"
   license "MIT"
   head "https://github.com/TaKO8Ki/frum.git"
+
+  if OS.mac?
+    url "https://github.com/TaKO8Ki/frum/releases/download/v#{version}/frum-v#{version}-x86_64-apple-darwin.tar.gz"
+    sha256 "57ab2eff76ac54049e7537c730eb62271258ee5646d6ff2a821ff44943ffa415"
+  elsif OS.linux?
+    url "https://github.com/TaKO8Ki/frum/releases/download/v#{version}/frum-v#{version}-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "a495ddf28e9cc7da20b9f823eff8cfa7248dc992a247ca5fb5c369cf5c8027ce"
+  end
 
   livecheck do
     url "https://github.com/TaKO8Ki/frum/releases/latest"
     regex(%r{href=.*?/tag/v?(\d+(?:\.\d+)+)["' >]}i)
   end
 
-  depends_on "rust" => :build
-
-  uses_from_macos "zlib"
-
   def install
-    system "cargo", "install", *std_cargo_args
+    bin.install "frum"
 
     (bash_completion/"frum").write Utils.safe_popen_read("#{bin}/frum", "completions", "--shell=bash")
-    (zsh_completion/"frum").write Utils.safe_popen_read("#{bin}/frum", "completions", "--shell=zsh")
+    (zsh_completion/"_frum").write Utils.safe_popen_read("#{bin}/frum", "completions", "--shell=zsh")
   end
 
   test do
