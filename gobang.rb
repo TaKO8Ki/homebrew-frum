5bccb73316ac1c60a055a0f048b2e86fd90e0958
Takayuki Maeda <takoyaki0316@gmail.com>
2021-09-08 11:11:04 +0900

    v0.1.0-alpha.3 gobang

--- gobang.rb
+++ gobang.rb
@@ -1,15 +1,15 @@
 class Gobang < Formula
   desc "Cross-platform TUI database management tool written in Rust"
   homepage "https://github.com/TaKO8Ki/gobang"
-  version "0.1.0-alpha.2"
+  version "0.1.0-alpha.3"
   license "MIT"
 
   if OS.mac?
-    url "https://github.com/TaKO8Ki/gobang/releases/download/v#{version}/gobang-v#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "4f5ae5223e910cc2e74513706af36f97043d99fdb7f22979383e19c99bc5cdbd"
+    url "https://github.com/TaKO8Ki/gobang/releases/download/v#{version}/gobang-#{version}-x86_64-apple-darwin.tar.gz"
+    sha256 "3d157b976a8bf60089f325a94bf4efbd9b8a7c5c97a166425698454f645c0ca2"
   elsif OS.linux?
-    url "https://github.com/TaKO8Ki/gobang/releases/download/v#{version}/gobang-v#{version}-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "ae8a1f2cce8458530f7dead6a30a899f1e0e8e0e793cdbceb45bf002b620af7f"
+    url "https://github.com/TaKO8Ki/gobang/releases/download/v#{version}/gobang-#{version}-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "3004bef9dd14a7810ba5de505b31bc04f2e501a53806571b52bd4a3715d851ef"
   end
 
   def install
