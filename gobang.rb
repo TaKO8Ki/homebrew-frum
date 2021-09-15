1f26e805f39b3682c6ff3e3c778c81246638e632
Takayuki Maeda <takoyaki0316@gmail.com>
2021-09-15 20:39:36 +0900

    v0.1.0-alpha.5 gobang

--- gobang.rb
+++ gobang.rb
@@ -1,15 +1,15 @@
 class Gobang < Formula
   desc "Cross-platform TUI database management tool written in Rust"
   homepage "https://github.com/TaKO8Ki/gobang"
-  version "0.1.0-alpha.4"
+  version "0.1.0-alpha.5"
   license "MIT"
 
   if OS.mac?
     url "https://github.com/TaKO8Ki/gobang/releases/download/v#{version}/gobang-#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "45daf8f56694b5e9da90656fb32cff25c94a7245254c9e4fd8884999dc243bd1"
+    sha256 "36679a7dac88788ad951ebdcdc9be304865a43c3ef2af0362dcb1984d2fde13a"
   elsif OS.linux?
     url "https://github.com/TaKO8Ki/gobang/releases/download/v#{version}/gobang-#{version}-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "3585d909c4f1e972b770456eb03101a11d846c59375a75362f5cb30b720f6d4c"
+    sha256 "29b8f517937cf19691c6e594a0789fe80a528fff05c346883af0e2950b9b90ef"
   end
 
   def install
